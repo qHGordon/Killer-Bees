949c58c52a66dc9e9852c95e33f8dcd46fae31a6
Justin <20jw8@queensu.ca>
2023-01-29 13:41:27 -0500

    Menu button added

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 var mySwiper = new Swiper ('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    naviagtion: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+    slidesPerView: 2,
+    spaceBetween: 50,
+    centeredSlides: true,
     scrollbar: {
       el: ".swiper-scrollbar",
       draggable: true,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      hide: true,
     },
   })
