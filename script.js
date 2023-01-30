f5a686e5b9731f6da1c602dc834324f398908c10
Justin <20jw8@queensu.ca>
2023-01-30 15:57:10 -0500

    Menu for A1 functional

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ var mySwiper = new Swiper ('.swiper-container', {
     slidesPerView: 2,
     spaceBetween: 50,
     centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      pauseOnMouseEnter: true,
+      disableOnInteraction: false,
+    },
     scrollbar: {
       el: ".swiper-scrollbar",
       draggable: true,
--- sidebar.js
+++ sidebar.js
@@ -1,1 +1,8 @@
-var menubtn = new onclick(".menuButton")
+function menuFunc() {
+    var body = document.body;
+    if (body.classList.contains("menuOpen")){
+        body.classList.remove("menuOpen");
+    }else{
+        body.classList.add("menuOpen")
+    }
+}
